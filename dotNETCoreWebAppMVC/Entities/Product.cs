abb2b1ad8e707bda0b4ae00fcbd49017b66f06ae
HungTranXD <105595047+HungTranXD@users.noreply.github.com>
2023-06-13 20:52:55 +0700

    first commit

---
+++ dotNETCoreWebAppMVC/Entities/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace dotNETCoreWebAppMVC.Entities
+
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Key] 
+        public int Id { get; set; } //Abstract property
+
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Column(TypeName = "text")]
+        public string Description { get; set; }
+
+        public int CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+    }
+}
